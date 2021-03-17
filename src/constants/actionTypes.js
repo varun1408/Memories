a3a5d9827ff40f431ceca4cc50d9c9a16b492c42
varun <varun@airoxa.in>
2021-03-17 12:09:16 +0530

    First Commit

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,8 @@
+export const CREATE = "CREATE";
+export const UPDATE = "UPDATE";
+export const DELETE = "DLETE";
+export const FETCH_ALL = "FETCH_ALL";
+export const LIKE = "LIKE";
+
+export const AUTH= "AUTH";
+export const LOGOUT= "LOGOUT";
